7d3f0d85e4ab041ddbab41d968fc781294cd03d7
DahlBrew <dahlia8rew@gmail.com>
2026-01-10 23:17:46 +0100

    The disc is finished

--- src/main/java/brew/modid/item/custom/ModItemGroups.java
+++ src/main/java/brew/modid/item/custom/ModItemGroups.java
@@ -2,7 +2,7 @@ package brew.modid.item.custom;
 
 import brew.modid.Brewsespresso;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.core.registries.Registries;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
@@ -11,17 +11,11 @@ import net.minecraft.world.item.ItemStack;
 
 public class ModItemGroups {
 
-    public static final ResourceKey<CreativeModeTab> MAIN_GROUP_KEY = ResourceKey.create(
-            Registries.CREATIVE_MODE_TAB,
-            Identifier.fromNamespaceAndPath(Brewsespresso.MOD_ID, "main_group")
-    );
-
-    public static final CreativeModeTab MAIN_GROUP = FabricItemGroup.builder()
-            .title(Component.translatable("itemgroup.brewsespresso.main_group"))
+    public static final ResourceKey<CreativeModeTab> CUSTOM_ITEM_GROUP_KEY = ResourceKey.create(BuiltInRegistries.CREATIVE_MODE_TAB.key(),
+            Identifier.fromNamespaceAndPath(Brewsespresso.MOD_ID, "item_group"));
+    public static final CreativeModeTab CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
             .icon(() -> new ItemStack(ModItems.BREWS_MISCHIEF))
-            .displayItems((parameters, output) -> {
-                output.accept(ModItems.BREWS_MISCHIEF);
-            })
+            .title(Component.translatable("itemGroup.brewsespresso"))
             .build();
 
     public static void initialize() {
