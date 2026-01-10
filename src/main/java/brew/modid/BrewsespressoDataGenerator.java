3c568ce06d3ff021b15324b095f48de4e4483cb3
DahlBrew <dahlia8rew@gmail.com>
2026-01-10 01:33:56 +0100

    Added the mod template

---
+++ src/main/java/brew/modid/BrewsespressoDataGenerator.java
@@ -0,0 +1,11 @@
+package brew.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BrewsespressoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
